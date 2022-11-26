b86aaa4cc8dadbfddeef7363118d7ad2dd077384
Rajshri Wagh <mahajan.rajshri16@gmail.com>
2022-11-26 10:58:32 +0530

    Add: Initial Commit

---
+++ src/main/java/com/hashtablepractice/App.java
@@ -0,0 +1,10 @@
+package com.hashtablepractice;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to implemented HashTable and Binary Tree" );
+    }
+}
